5692430702199704507006eeaeb1c708cbef33ee
Tshepo Tsilo <tshepo9099@gmail.com>
2022-06-27 21:03:34 +0200

    feat: added changeImg method of the ImageSlider class

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 // Import ImageSlider Class Library
 import ImageSilder from "./lib/ImageSlider.js";
 
-console.log("test");
-
 // Initialize the image slider
 const imgSlider = new ImageSilder({
   imgSliderIdentifier: "#hero-img-slider",
