63a898f626d28232c72e630126e724c1e0bc7a65
Tshepo Tsilo <tshepo9099@gmail.com>
2022-07-11 17:31:33 +0200

    refactor: created BaseSection component class

---
+++ src/js/lib/base/BaseSection.component.js
@@ -0,0 +1,152 @@
+// ----------------------------------------------------------------------
+// TYPES DEFINITION
+// ----------------------------------------------------------------------
+
+/**
+ * HTML element attributes
+ * @typedef {Object} ElementAttributes
+ * @property {string} name  attribute name
+ * @property {string} value attribute name
+ */
+
+/**
+ * @typedef {Object} Image
+ * @property {string} src  img tag src attributes
+ * @property {string} altText  img tag alt attributes
+ * @property {ElementAttributes[] | undefined} attributes  img tag attributes
+ */
+
+/**
+ * @typedef {import("../pages/home/components/Card.component").UserCard} UserCard
+ */
+
+/**
+ * @typedef {import("./BaseButton.component").ButtonModel} CtaBtn
+ */
+
+/**
+ * @typedef {Object} APIContent
+ * @property {string[] | undefined} string
+ * @property {UserCard | undefined} userCard
+ * @property {CtaBtn | undefined} ctaBtn
+ * @property {"string" | "userCard" | "ctaBtn"} __type
+ */
+
+/**
+ * @typedef {Object} ContentCard
+ * @property {APIContent | undefined} header
+ * @property {APIContent} body
+ * @property {APIContent | undefined} footer
+ */
+
+/**
+ * @typedef {Object} HeroSection
+ * @property {ContentCard} heroContent
+ * @property {Image[]} imgSlider
+ * @property {Image[]} clientLogos
+ */
+
+/**
+ * @typedef {Object} AboutUsSection
+ * @property {ContentCard} statement
+ * @property {ContentCard} msgFromCEO
+ */
+
+/**
+ * @typedef {Object} CaseStudy
+ * @property {Image} caseImg
+ * @property {ContentCard} caseDetails
+ * @property {ContentCard} msgFromClient
+ */
+
+/**
+ * @typedef {Object} SectionTitle
+ * @property {string} title
+ * @property {string} description
+ */
+
+/**
+ * @typedef {Object} ProjectsSection
+ * @property {SectionTitle} sectionHeader
+ * @property {CaseStudy[]} caseStudies
+ */
+
+/**
+ * @typedef {Object} OurServicesSection
+ * @property {SectionTitle} sectionHeader
+ * @property {ContentCard[]} ourServices
+ * @property {ContentCard} letsGetStarted
+ */
+
+/**
+ * @typedef {Object} APIResponse
+ * @property {HeroSection} heroSection
+ * @property {AboutUsSection} aboutUsSection
+ * @property {ProjectsSection} projectsSection
+ * @property {OurServicesSection} ourServicesSection
+ */
+
+// ----------------------------------------------------------------------
+// CLASS DEFINITION
+// ----------------------------------------------------------------------
+
+export default class BaseSection {
+  /**
+   * @property
+   * @type {APIResponse}
+   */
+  model;
+
+  /**
+   * @property
+   * @type {any[]}
+   */
+  components = [];
+
+  /**
+   * @constructor
+   * @param {APIResponse} model
+   */
+  constructor(model) {
+    this.model = model;
+  }
+
+  async init() {
+    // Initialize child components
+    await this.initComponents();
+  }
+
+  /**
+   * @method
+   * @protected
+   */
+  async initComponents() {}
+
+  /**
+   * @method
+   * @public
+   */
+  async initEvents() {
+    // Initialize child component events
+    this.components.map((component) => {
+      component.initEvents();
+    });
+  }
+
+  get template() {
+    return renderTemplate();
+  }
+
+  set addComponent(component) {
+    this.components.push(component);
+  }
+
+  /**
+   * @returns {string} a HTML string of the component
+   * @method
+   * @protected
+   */
+  renderTemplate() {
+    throw "Base Section Component Error: method `renderTemplate()` has no implementation";
+  }
+}
